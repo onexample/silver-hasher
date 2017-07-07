371cbb9b285f7a25a52ed051dd2f3c4a28fe0895
onexample <onexample@gmail.com>
2017-07-07 17:01:12 +0300

    Changed test messages for isPrime

--- src/func/isPrime/isPrime.spec.ts
+++ src/func/isPrime/isPrime.spec.ts
@@ -5,24 +5,24 @@ import { isPrime } from './isPrime'
 describe('Function: isPrime(n)', () =>
 {
 
-    it('should return false if 0 passed', () =>
+    it('should return false if n=0', () =>
     {
         assert.equal(isPrime(0), false, '0 is not a prime number');
     });
 
-    it('should return false if 1 passed', () =>
+    it('should return false if n=1', () =>
     {
         assert.equal(isPrime(1), false, '1 is not a prime number');
     });
 
-    it('should return true if 17 passed', () =>
+    it('should return true if n=17', () =>
     {
         assert.equal(isPrime(17), true, '17 is prime number');
     });
 
-    it('should return false if 10000000000000 passed', () =>
+    it('should return false if n=10000000000000', () =>
     {
-        assert.equal(isPrime(10000000000000), false, '10000000000000 is prime number');
+        assert.equal(isPrime(10000000000000), false, '10000000000000 is not prime number');
     });
 
 });
