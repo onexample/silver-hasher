e8777ac93bf9cf78ac988b94204bbb132e02ac86
onexample <onexample@gmail.com>
2017-07-25 16:23:47 +0300

    removed old config

--- webpack.config.js
+++ webpack.config.js
@@ -22,27 +22,4 @@ const config = {
     }
 };
 
-const example = {
-    name: 'example',
-    entry: './example/main.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        filename: `./example/main.js`,
-        path: __dirname
-    }
-}
-
-
-module.exports = [config, example];
+module.exports = config;
